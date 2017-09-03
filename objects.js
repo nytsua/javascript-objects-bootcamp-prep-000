009f8bbc7ce8827de94eeb95593550389f881a37
Austyn Trace <atrace@gmail.com>
2017-09-03 23:40:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,6 @@
 var playlist = {};
-playlist['Of monsters and men'] = 'Little Talks'
+playlist['Of monsters and men'] = 'Little Talks';
+
+function updatePlaylist(obj, artist, song){
+  return Object.assign({}, obj, {artist: song});
+}
